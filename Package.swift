3d9f5cb37bd8521cf52e731546b8cbbf091ccc6c
thysmesi <77686907+thysmesi@users.noreply.github.com>
2021-10-22 09:33:35 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Surrow",
-            path: "./Swift/Sources/Surrow",
+            path: "Swift/Sources/Surrow",
             dependencies: []),
         .testTarget(
             name: "SurrowTests",
-            path: "./Swift/Tests/SurrowTests",
+            path: "Swift/Tests/SurrowTests",
             dependencies: ["Surrow"]),
     ]
 )
