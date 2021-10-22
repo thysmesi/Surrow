611d6f48d33bc4b1837a1002000570cfb47d1bb2
thysmesi <77686907+thysmesi@users.noreply.github.com>
2021-10-22 09:46:51 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Surrow",
-            targets: ["Box","Circle","Collidable","Line","Point","Polygon","Segment","Size","Vector"]),
+            targets: ["Surrow"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,41 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Box",
+            name: "Surrow",
             dependencies: [],
             path: "Swift/Sources/Surrow"),
-        .target(
-            name: "Circle",
-            dependencies: [],
-            path: "Swift/Sources/Surrow"),
-        .target(
-            name: "Collidable",
-            dependencies: ["Box","Circle","Line","Point","Polygon","Segment","Size","Vector"],
-            path: "Swift/Sources/Surrow"),
-        .target(
-            name: "Line",
-            dependencies: [],
-            path: "Swift/Sources/Surrow"),
-        .target(
-            name: "Point",
-            dependencies: [],
-            path: "Swift/Sources/Surrow"),
-        .target(
-            name: "Polygon",
-            dependencies: [],
-            path: "Swift/Sources/Surrow"),
-        .target(
-            name: "Segment",
-            dependencies: [],
-            path: "Swift/Sources/Surrow"),
-        .target(
-            name: "Size",
-            dependencies: [],
-            path: "Swift/Sources/Surrow"),
-        .target(
-            name: "Vector",
-            dependencies: ["Box","Circle","Collidable","Line","Point","Polygon","Segment","Size"],
-            path: "Swift/Sources/Surrow")
-        
     ]
 )
