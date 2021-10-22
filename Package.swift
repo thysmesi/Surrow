856069775c8a50965c911208469bcef65c34d84b
thysmesi <77686907+thysmesi@users.noreply.github.com>
2021-10-22 09:42:16 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Surrow",
-            targets: ["Surrow"]),
+            targets: ["Box","Circle","Collidable","Line","Point","Polygon","Segment","Size","Vector"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
