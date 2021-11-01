1bd75326baa59f9c9d3747fa25d8367a048904fc
thysmesi <77686907+thysmesi@users.noreply.github.com>
2021-11-01 12:33:28 -0600

    Update Polygon.swift

--- Swift/Sources/Surrow/Polygon.swift
+++ Swift/Sources/Surrow/Polygon.swift
@@ -215,6 +215,17 @@ public class Polygon: CustomStringConvertible, Hashable, Codable {
     
     
     // MARK: - Testing
+    func intercects(_ other: Polygon) -> [Point] {
+        var intercects: [Point] = []
+        for segment1 in sides {
+            for segment2 in other.sides {
+                if let intercect = segment1.intercects(segment2) {
+                    intercects.append(intercect)
+                }
+            }
+        }
+        return intercects
+    }
     
     
     // MARK: - Initializers
